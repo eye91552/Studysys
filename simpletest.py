589dfcb96a3150a23360fca6354bd732d9ee72d5
Shin jong hyun <eye9155@naver.com>
2022-12-29 17:01:42 +0900

    간단한 웹서버

--- simpletest.py
+++ simpletest.py
@@ -1,6 +1,18 @@
-a = 10
+#필요한 모듈을 단계적으로 import
+import os
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+ 
+# static 기능 추가
+app = Flask(__name__)
+ 
+# 테스트 기능
+@app.route('/hello')
+def hello_Flask():
+    return 'Hello, Flask'
 
-b = 20
+if __name__ == '__main__':
+    app.run(
+    host="0.0.0.0",
+    port=7777,
+    debug=True)
 
-c = a+b
-print(c)
